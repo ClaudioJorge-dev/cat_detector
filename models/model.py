9beb75dbd3d9dbd1a351ad7f7ed8623e7b896421
Cláudio Jorge <aluno24871@ipt.pt>
2026-04-17 00:28:40 +0100

    some model fixes and cat_classifier.pth model file
    - better model aux functions

--- models/model.py
+++ models/model.py
@@ -1,16 +1,25 @@
+from sys import path
+
 import torch
 from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
 
-model = None
-device = None
 
-def load_model():
-    global model, device
-
-    if model is None:
-        model = mobilenet_v2(weights=MobileNet_V2_Weights.IMAGENET1K_V1)
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model.to(device)
-        model.eval()
-
+"""
+Used to create model for training and loading the model for inference.
+"""
+def create_model():
+    model = mobilenet_v2(weights=MobileNet_V2_Weights.IMAGENET1K_V1)
+    model.classifier[1] = torch.nn.Linear(1280,12) # Adjust the final layer to output 12 classes (for 12 cat breeds)
     return model
+
+
+def load_model(model, path, device):
+    model.load_state_dict(torch.load(path, map_location=device))
+    return model
+
+def get_inference_model(path):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = create_model()
+    model.load_model(model, path, device)
+    model.eval()
+    return model, device
