537459abaa3864f462800aab09fcde3f2d9f3e9d
Cláudio Jorge <aluno24871@ipt.pt>
2026-04-17 00:27:52 +0100

    moved files to respective folder

--- models/ClassificationModel.py
+++ models/ClassificationModel.py
@@ -2,12 +2,12 @@ import cv2
 import torch
 from torchvision import transforms
 from utils import get_cat_breed_from_probs
-from models.model import load_model
+from models.model import  get_inference_model
 from pathlib import Path
 
 class ClassificationModel:
     def __init__(self):
-        self.model = load_model()
+        self.model, self.device = get_inference_model('cat_classifier.pth') # Load the trained model for inference
         self.transform = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize((224,224)),
@@ -18,9 +18,8 @@ class ClassificationModel:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = self.transform(image).unsqueeze(0)
         
-        device = next(self.model.parameters()).device
-        image = image.to(device)
         with torch.no_grad():
             outputs = self.model(image)
             probs = torch.nn.functional.softmax(outputs, dim=1)
+            
         return get_cat_breed_from_probs(probs[0])
