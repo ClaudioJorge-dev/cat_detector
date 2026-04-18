49f68682b87c43fe896a20522c3dc21ab5b6eb87
Cláudio Jorge <aluno24871@ipt.pt>
2026-04-19 00:24:41 +0100

    allow to set a training mode which freezes initial layers on model creation

--- models/model.py
+++ models/model.py
@@ -7,9 +7,19 @@ from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
 """
 Used to create model for training and loading the model for inference.
 """
-def create_model():
+def create_model(training = False):
     model = mobilenet_v2(weights=MobileNet_V2_Weights.IMAGENET1K_V1)
     model.classifier[1] = torch.nn.Linear(1280,12) # Adjust the final layer to output 12 classes (for 12 cat breeds)
+    
+    if training:
+        # Freeze everything first
+        for param in model.features.parameters():
+            param.requires_grad = False
+
+        # Unfreeze last 20 blocks
+        for param in model.features[-20:].parameters():
+            param.requires_grad = True
+    
     return model
 
 
